0fcb725949395c233a91d16b7f53df2354b8ccda
Zielonyy <zielak134@gmail.com>
2023-02-26 19:40:40 +0100

    18 - Extract a PHP Database class

--- index.php
+++ index.php
@@ -2,4 +2,14 @@
 
 require 'functions.php';
 
-require 'router.php';
+//require 'router.php';
+
+// connect to our MySQL database
+
+$dsn = "mysql:host=localhost;port=3306;dbname=demo;charset=utf8mb4";
+
+$pdo = new PDO($dsn, 'root');
+$statement = $pdo->prepare('select * from posts');
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
